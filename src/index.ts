cf2994ad451662daf90fa169ea338b66ade082cf
Katherina Kaliaha <mellinda077@mail.ru>
2022-08-21 20:15:59 +0300

    feat: add events for auth window #34

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import './styles/index.scss';
 import renderApp from './views/renderMenu';
 import './controllers/controller';
 import getListenerBurgerButton from './controllers/burgerController';
+import './controllers/authController';
 
 renderApp();
 getListenerBurgerButton();
