d2fb0fac1440bc8ed75328affac5fcbf5ac15037
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2022-08-28 17:19:08 +0300

    feat: add keyboard event for Audio-Game

--- src/controllers/startAudioGame.ts
+++ src/controllers/startAudioGame.ts
@@ -5,6 +5,22 @@ import saveWordLocal from './saveWordsLocal';
 import logicAudioGame from './logicAudioGame';
 import changeHashPage from '../model/hashPage';
 
+export const eventStartGame = (event: KeyboardEvent) => {
+  const gameWrapper = document.querySelector('.audio-game-wrapper') as HTMLElement;
+  const keyCode = event.key;
+  switch (keyCode) {
+    case 'Enter':
+      window.removeEventListener('keydown', eventStartGame);
+      saveWordLocal();
+      gameWrapper.innerHTML = htmlGamePageAudioCall;
+      logicAudioGame();
+      changeHashPage('game-audio/start');
+      break;
+    default:
+      break;
+  }
+};
+
 const startAudioGame = (): void => {
   const serverPath = serverRequests.baseUrl;
   const startButton = document.querySelector('.audio-game-start-button') as HTMLElement;
@@ -17,6 +33,7 @@ const startAudioGame = (): void => {
       changeHashPage('game-audio/start');
     });
   }
+  window.addEventListener('keydown', eventStartGame);
   playSound(gameWrapper, serverPath);
 };
 
