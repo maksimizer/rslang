4f4d852e948697d847bded0ec5a7a242efc80864
Katherina Kaliaha <mellinda077@mail.ru>
2022-08-30 21:52:01 +0300

    feat: add method for find the longest right answers

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -4,7 +4,7 @@ import { renderMainContent } from '../views/renderMenu';
 import renderPageGames from '../views/renderPageGames';
 import textbookController from './textbookController';
 
-document.addEventListener('click', (event: Event): void => {
+document.addEventListener('click', async (event: Event): Promise<void> => {
   const namePage = document.querySelector('.name-page') as HTMLDivElement;
 
   if ((event.target as HTMLBodyElement).classList.contains('menu-home')) {
@@ -26,6 +26,13 @@ document.addEventListener('click', (event: Event): void => {
     changeHashPage('games');
     namePage.innerHTML = getNamePage();
     renderPageGames();
+    // const userData = JSON.parse(localStorage.getItem('user') as string);
+    // const status = await serverRequests.getUserResponse(userData.userId, userData.token);
+    // console.log(userData, status);
+    // if (status !== 200) {
+    //   const newUser = await serverRequests.getUserTokens(userData.id, userData.refreshToken);
+    //   localStorage.setItem('user', JSON.stringify(newUser));
+    // }
   }
 
   if ((event.target as HTMLBodyElement).classList.contains('menu-statistics')) {
