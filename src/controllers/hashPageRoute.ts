046c7f78758acb654a58b3d40ffcd8a876ae3666
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2022-09-04 15:41:48 +0300

    fix: replay request when switch beetwin pages

--- src/controllers/hashPageRoute.ts
+++ src/controllers/hashPageRoute.ts
@@ -14,73 +14,82 @@ import { IAuth } from '../types/interface';
 import startAudioGameBook from './startAudioGameBook';
 import renderPage404 from '../views/render404Page';
 
-const hashPageRoute = () => {
-  window.addEventListener('hashchange', async () => {
-    const hash = window.location.hash.slice(1);
-    const namePage = document.querySelector('.name-page') as HTMLDivElement;
-    switch (hash) {
-      case 'game-audio':
-        drawAudioGame();
-        startAudioGame();
-        fullScreen(document.querySelector('.audio-game-wrapper') as HTMLElement);
+async function hashFunction() {
+  const hash = window.location.hash.slice(1);
+  const namePage = document.querySelector('.name-page') as HTMLDivElement;
+  switch (hash) {
+    case 'game-audio':
+      drawAudioGame();
+      startAudioGame();
+      fullScreen(document.querySelector('.audio-game-wrapper') as HTMLElement);
+      document.querySelector('.footer')?.classList.add('hidden');
+      window.removeEventListener('hashchange', () => hashFunction());
+      break;
+    case 'game-audio/start':
+      document.querySelector('.footer')?.classList.add('hidden');
+      break;
+    case 'games':
+      namePage.innerHTML = getNamePage();
+      renderPageGames();
+      document.querySelector('.footer')?.classList.remove('hidden');
+      window.removeEventListener('hashchange', () => hashFunction());
+      break;
+    case 'game-sprint':
+      {
+        drawSprintGame();
+        const groupAndPage = [{ key: 'group', value: '0' }, { key: 'page', value: 'null' }];
+        localStorage.setItem('sprintGroupAndPage', JSON.stringify(groupAndPage));
+        fullScreen(document.querySelector('.sprint-game-wrapper') as HTMLElement);
+        closeGameWindow(document.querySelector('.sprint-game-close_span') as HTMLButtonElement);
         document.querySelector('.footer')?.classList.add('hidden');
-        break;
-      case 'game-audio/start':
-        document.querySelector('.footer')?.classList.add('hidden');
-        break;
-      case 'games':
+        window.removeEventListener('hashchange', () => hashFunction());
+      }
+      break;
+    case 'main':
+      {
         namePage.innerHTML = getNamePage();
-        renderPageGames();
+        const mainContainer = document.querySelector('.main') as HTMLElement;
+        mainContainer.innerHTML = renderMainContent();
         document.querySelector('.footer')?.classList.remove('hidden');
-        break;
-      case 'game-sprint':
-        {
-          drawSprintGame();
-          const groupAndPage = [{ key: 'group', value: '0' }, { key: 'page', value: 'null' }];
-          localStorage.setItem('sprintGroupAndPage', JSON.stringify(groupAndPage));
-          fullScreen(document.querySelector('.sprint-game-wrapper') as HTMLElement);
-          closeGameWindow(document.querySelector('.sprint-game-close_span') as HTMLButtonElement);
-          document.querySelector('.footer')?.classList.add('hidden');
-        }
-        break;
-      case 'main':
-        {
-          namePage.innerHTML = getNamePage();
-          const mainContainer = document.querySelector('.main') as HTMLElement;
-          mainContainer.innerHTML = renderMainContent();
-          document.querySelector('.footer')?.classList.remove('hidden');
-        }
-        break;
-      case 'book':
-        namePage.innerHTML = getNamePage();
-        textbookController.renderTextbookPage();
-        textbookController.renderWords();
-        textbookController.addEventListeners();
-        startAudioGameBook();
-        document.querySelector('.footer')?.classList.remove('hidden');
-        break;
-      case 'statistics':
-        namePage.innerHTML = getNamePage();
+        window.removeEventListener('hashchange', () => hashFunction());
+      }
+      break;
+    case 'book':
+      namePage.innerHTML = getNamePage();
+      textbookController.renderTextbookPage();
+      textbookController.renderWords();
+      textbookController.addEventListeners();
+      startAudioGameBook();
+      document.querySelector('.footer')?.classList.remove('hidden');
+      window.removeEventListener('hashchange', () => hashFunction());
+      break;
+    case 'statistics':
+      namePage.innerHTML = getNamePage();
 
-        if (localStorage.getItem('auth') === 'true') {
-          const user: IAuth = await JSON.parse(localStorage.getItem('user') as string);
-          const userStat = await JSON.parse(localStorage.getItem('statistic') as string);
-          delete userStat.id;
-          await serverRequests.updateUserStatistic(user.userId, user.token, userStat);
-          const statistic = await serverRequests.getUsersStatistic(user.userId, user.token);
-          delete statistic.id;
-          const data = getStatisticsForRender();
-          renderUserPageStatistics(userPageStatisticsView(data));
-        } else {
-          renderPageStatistics();
-        }
-        document.querySelector('.footer')?.classList.remove('hidden');
-        break;
-      default:
-        renderPage404();
-        break;
-    }
-  });
+      if (localStorage.getItem('auth') === 'true') {
+        const user: IAuth = await JSON.parse(localStorage.getItem('user') as string);
+        const userStat = await JSON.parse(localStorage.getItem('statistic') as string);
+        delete userStat.id;
+        await serverRequests.updateUserStatistic(user.userId, user.token, userStat);
+        const statistic = await serverRequests.getUsersStatistic(user.userId, user.token);
+        delete statistic.id;
+        const data = getStatisticsForRender();
+        renderUserPageStatistics(userPageStatisticsView(data));
+      } else {
+        renderPageStatistics();
+      }
+      document.querySelector('.footer')?.classList.remove('hidden');
+      window.removeEventListener('hashchange', () => hashFunction());
+      break;
+    default:
+      renderPage404();
+      window.removeEventListener('hashchange', () => hashFunction());
+      break;
+  }
+}
+
+const hashPageRoute = () => {
+  window.addEventListener('hashchange', () => hashFunction(), true);
 };
 
 export default hashPageRoute;
